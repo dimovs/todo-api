621065e4354a818e77e6527f017f400a067637e7
Dmitriy Ovsyannikov <dmitriy.ovsyannikov@semrush.com>
2025-07-12 18:30:25 +0200

    add todos handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/lib/pq"
 	"log"
@@ -16,9 +17,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to open DB: %w", err)
 	}
+
 	if err = db.Ping(); err != nil {
 		log.Fatalf("DB is unreachable: %w", err)
 	}
+
 	fmt.Println("Connected to PostgreSQL")
 
 	// http server
@@ -29,10 +32,46 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
+	mux.HandleFunc("/todos", getTodosHandler(db))
+
 	fmt.Println("Starting service on http://localhost:8080")
 
 	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatalf("Server failed: %w", err)
+	}
+}
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+	CreatedAt string `json:"created_at"`
+}
+
+func getTodosHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("select id, title, completed, created_at from todos order by id")
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		defer rows.Close()
+
+		todos := []Todo{}
+		for rows.Next() {
+			var todo Todo
+			err := rows.Scan(&todo.ID, &todo.Title, &todo.Completed, &todo.CreatedAt)
+			if err != nil {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+			todos = append(todos, todo)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(w).Encode(todos)
 	}
 }
