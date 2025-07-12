05a8e5755fd7c3c4b15ff40ae7e0e8fe2e11e41f
Dmitriy Ovsyannikov <dmitriy.ovsyannikov@semrush.com>
2025-07-12 17:39:11 +0200

    add db

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// database
+	dsn := "postgres://dimovs@localhost:5432/tododb?sslmode=disable"
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Failed to open DB: %w", err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Fatalf("DB is unreachable: %w", err)
+	}
+	fmt.Println("Connected to PostgreSQL")
+
+	// http server
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +31,7 @@ func main() {
 
 	fmt.Println("Starting service on http://localhost:8080")
 
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatalf("Server failed: %w", err)
 	}
